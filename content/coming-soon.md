17ff553192f4255d2ef30eec6db18c7b60203baf
mchwebapps <mchwebapps@hotmail.com>
2025-03-19 23:37:42 +0100

    feat(pages): added test 'Coming Soon' page with markdown content

---
+++ content/coming-soon.md
@@ -0,0 +1,6 @@
+---
+title: 'Coming soon...'
+description: '...'
+---
+
+[docs](https://content.nuxt.com/docs/getting-started/installation){target="\_blank"}
---
+++ pages/coming-soon.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+const { data: example } = await useAsyncData(() =>
+  queryCollection('content').path('/coming-soon').first()
+)
+useSeoMeta({
+  title: example.value?.title,
+  description: example.value?.description,
+})
+</script>
+
+<template>
+  <div>
+    <USeparator
+      color="neutral"
+      type="dashed"
+      :label="example?.title ? example.title : '...'"
+      class="pt-4 pb-6"
+    />
+
+    <ContentRenderer v-if="example" :value="example" />
+    <div v-else>Page not found</div>
+  </div>
+</template>
