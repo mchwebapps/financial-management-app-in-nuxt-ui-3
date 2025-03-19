ea83ae6e8ae8cad2354661277f6fae482b6157c8
mchwebapps <mchwebapps@hotmail.com>
2025-03-19 23:33:32 +0100

    feat(pages): update app home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,22 @@
+<script setup lang="ts">
+const loaded = ref(false)
+
+onMounted(() => (loaded.value = true))
+</script>
+
 <template>
-  <div class="p-2.5">
-    <h2>Coming soon...</h2>
+  <div>
+    <USeparator
+      color="neutral"
+      type="dashed"
+      :label="loaded ? 'Wydatki' : '...'"
+      class="pt-4 pb-6"
+    />
+
+    <UContainer class="px-2.5 sm:px-4 lg:px-8">
+      <div>
+        <FormsExpenseForm />
+      </div>
+    </UContainer>
   </div>
 </template>
